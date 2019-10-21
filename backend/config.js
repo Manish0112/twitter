88e72eae5a6e4737584784ab4be6cf5288ffef93
Manish <manishlokhande96@gmail.com>
2019-10-21 01:37:50 -0700

    Twitter api

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    consumer_key:         'BLPQwlCPBc7Daa5GPEueAupgx',
+    consumer_secret:      'HkaZgW89y9PWP7BxZUpjsp7yORJRnQrl2kUKyhCHFUu9xDIsM2',
+    access_token:         '1185691690465554433-JLEdanruKA4YqcnrRiKSfCQDprHmIu',
+    access_token_secret:  '7PpPfNNZAbuTcBb2TCpcUMA3mubSCg2LPZf3hTjeYaiW6',
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    strictSSL:            true,     // optional - requires SSL certificates to be valid.
+  
+}
